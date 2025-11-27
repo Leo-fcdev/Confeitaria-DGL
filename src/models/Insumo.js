dd7fd1b497ac69e5af25ac895b81b7f131086923
brguzz <gustavoguzz@outlook.com.br>
2025-11-27 20:12:10 -0300

    atualização dos arquivos anteriores

--- src/models/Insumo.js
+++ src/models/Insumo.js
@@ -7,7 +7,7 @@ const InsumoSchema =  new mongoose.Schema({
     },
     unidade: {
         type: String,
-        required: true
+        requerid: true
     },
     preco: {
         type: Number,
